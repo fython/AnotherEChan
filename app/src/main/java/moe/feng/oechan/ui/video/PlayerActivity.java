7b5347d875170f71e54b147531ab86dee75fa18c
Fung <fython@163.com>
2016-08-20 16:47:57 +0800

    PlayerActivity: Create
    Signed-off-by: Fung <fython@163.com>

---
+++ app/src/main/java/moe/feng/oechan/ui/video/PlayerActivity.java
@@ -0,0 +1,14 @@
+package moe.feng.oechan.ui.video;
+
+import android.os.Bundle;
+
+import moe.feng.oechan.ui.common.AbsActivity;
+
+public class PlayerActivity extends AbsActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
